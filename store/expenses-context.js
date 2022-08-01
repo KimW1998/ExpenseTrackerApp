c3bc13953f4f68d5d7400ffc4fc07f364294f769
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-08-01 13:12:37 +0200

    made the context component available to what ever component needs it

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -109,7 +109,14 @@ function ExpensesContextProvider({ children }) {
     dispatch({ type: "UPDATE", payload: { id, data: expenseData } });
   }
 
-  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+  const value = { 
+    expenses: expensesState, 
+    addExpenses: addExpenses,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
+};
+
+  return <ExpensesContext.Provider value={value}>{children}</ExpensesContext.Provider>;
 }
 
 export default ExpensesContextProvider;
